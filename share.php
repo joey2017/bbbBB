d5157a8dc88e90433fed5fe61842dc7dd441113d
joey <gxlclizongjun@163.com>
2019-03-15 10:17:53 +0800

    配置

--- share.php
+++ share.php
@@ -11,5 +11,5 @@
 
 return [
   //服务器  配置顺序：0、落地 1、群跳板 2、圈跳板 3、公众号ID 4、公众号秘钥
-'qdtyh0.cn' => ['s4.app1107984616.qqopenapp.com','s4.app1107984616.qqopenapp.com','wx117c44d26304315f','f10bf7dba379d0ecd9febc6d3ba8eb43'],
+'yiukeo.cn' => ['s2.app1108264292.qqopenapp.com','s4.app1108264292.qqopenapp.com','wxe7547fe786b137b3','6f9a3c16f7c8b183e71416722662a4f2'],
 ]; 
